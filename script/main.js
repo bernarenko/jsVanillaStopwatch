76407566486a68ac529627607fa0f74fa21672aa
Evgeniy Bernarenko <evgeniyjean@gmail.com>
2022-05-31 07:31:25 -0300

    refactured code to use Date.now() and to make it more precise

--- script/main.js
+++ script/main.js
@@ -1,27 +1,37 @@
 var seconds = 0;
 
+var presentTime = 0;
+var elapesedTime = 0;
+var reTimed = 0;
+
 
 function startedClock(){
-    if (seconds == 0){
+    if(presentTime != 0){
+        presentTime = Date.now()
+        myInterval = setInterval(restartingClock, 1000)
+    } else {
+        presentTime = Date.now()
         myInterval = setInterval(startingClock, 1000)
-        
         document.querySelector('p#marcador').innerHTML = '0'
-        document.querySelector('input#butComecar').onclick = ''
-    } else{
-        myInterval = setInterval(startingClock, 1000)
-        
         document.querySelector('input#butComecar').onclick = ''
     }
 }
 
 function startingClock(){
-    seconds++
+    elapesedTime = (Date.now() - presentTime) / 1000
     
-    document.querySelector('p#marcador').innerHTML = seconds 
+    document.querySelector('p#marcador').innerHTML = elapesedTime 
+}
+
+function restartingClock(){
+    reTimed = (Date.now() - presentTime) / 1000
+
+    document.querySelector('p#marcador').innerHTML = elapesedTime + reTimed
 }
 
 function stoppedClock(){
     clearInterval(myInterval)
+    elapesedTime = elapesedTime + reTimed
     
     document.querySelector('input#butComecar').onclick = startedClock
     document.querySelector('input#butComecar').value = 'Continuar'
