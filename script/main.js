f8bb8c714ac4ca9e8fc27514f68d8583a2e33f17
Evgeniy Bernarenko <evgeniyjean@gmail.com>
2022-05-31 07:12:06 -0300

    fixed identation

--- script/main.js
+++ script/main.js
@@ -19,13 +19,13 @@ function startingClock(){
     
     document.querySelector('p#marcador').innerHTML = seconds 
 }
+
 function stoppedClock(){
     clearInterval(myInterval)
     
     document.querySelector('input#butComecar').onclick = startedClock
     document.querySelector('input#butComecar').value = 'Continuar'
 }
-
 
 function resettedClock(){
     clearInterval(myInterval)
